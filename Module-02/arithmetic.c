1e661c473f7367e4320a695ad0ddddc4d00300c2
Ashab Uddin <ashabuddin47@gmail.com>
2025-10-05 23:05:48 +0600

    Learn 4 type of arithmetic operatior

--- Module-02/arithmetic.c
+++ Module-02/arithmetic.c
@@ -6,7 +6,16 @@ int main(){
     printf("Enter value of b: ");
     scanf("%d",&b);
     sum = a+b;
-    printf("Sum of a and b is: %d",sum);
+    int mul = a*b;
+    int sub = a-b;
+    int div = a/b;
+    printf("Sum of a and b is: %d\n",sum);
+    printf("multiplication of a and b is: %d\n",mul);
+    printf("subtraction of a and b is: %d\n",sub);
+    printf("divition of a and b is: %d\n",div);
+    
+    
+
     return 0;
 
 }
