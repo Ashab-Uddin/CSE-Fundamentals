155a46ae2573239887a0e4ae75b31869d89ad05b
Ashab Uddin <ashabuddin47@gmail.com>
2025-10-17 02:41:00 +0600

    Solve Q_Digits problem from codeforces

--- Module-06/tempCodeRunnerFile.c
+++ Module-06/tempCodeRunnerFile.c
@@ -1,17 +1,7 @@
-#include<stdio.h>
-int main(){
-   int p = 1999;
-   int n,x;
-   scanf("%d",&n);
-   for(int i=1;i<=n;i++){
-        scanf("%d",&x);
-        if(x==1999){
-            printf("Correct");
-            break;
-        }
-        else{
-            printf("Wrong\n");
-        }
-   }
-    return 0;
-}
+for(int i=1;i<=n;i++){
+//         scanf("%d",&x);
+//         x = m;
+//         printf("%d \n",x%10);
+
+
+//    }
