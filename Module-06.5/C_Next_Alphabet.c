037d0cb63d3b82b79d40d36325c9fea85c1771f5
Ashab Uddin <ashabuddin47@gmail.com>
2025-10-17 23:15:59 +0600

    Solve a k_divisors problem from codeforces

---
+++ Module-06.5/C_Next_Alphabet.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+   char ch;
+   scanf("%c",&ch);
+   if(ch>='a' && ch<'z'){
+        printf("%c",ch+1);
+   }
+   if(ch=='z'){
+    printf("%c",ch-25);
+   }
+    return 0;
+}
