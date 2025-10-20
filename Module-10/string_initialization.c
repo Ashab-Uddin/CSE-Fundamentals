d06c3c834cdfaac8904dd1bda046ef84fb3843f5
Ashab Uddin <ashabuddin47@gmail.com>
2025-10-21 02:04:32 +0600

    String initialization

---
+++ Module-10/string_initialization.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    //method-1
+    //char s[20] = {'a', 's', 'h', 'a', 'b', 'u', 'd', 'd', 'i', 'n'};
+    
+    //Method-2
+    char s[20] = "Ashab Uddin";
+    printf("%s",s);
+    return 0;
+}
