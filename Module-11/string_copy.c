d8974fc644e324088e5163db683059d80a3ea7e2
Ashab Uddin <ashabuddin47@gmail.com>
2025-10-23 17:12:19 +0600

    learn string copy

--- Module-11/string_copy.c
+++ Module-11/string_copy.c
@@ -5,7 +5,7 @@ int main()
     char a[101],b[101];
     scanf("%s %s",&a,&b);
     
-    for(int i=0;i<strlen(a);i++){
+    for(int i=0;i<=strlen(b);i++){
         a[i] = b[i];
     }
     printf("%s",a);
