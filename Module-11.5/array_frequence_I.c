1335fa18203b7455c230855766dc867ae9964562
Ashab Uddin <ashabuddin47@gmail.com>
2025-10-24 10:01:28 +0600

    learn find the frequency of an array

---
+++ Module-11.5/array_frequence_I.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    int cnt0 =0;
+    for(int i=0;i<n;i++){
+        if(a[i] == 0){
+            cnt0++;
+        }
+    }
+    printf("%d -> %d\n",0,cnt0);
+    return 0;
+}
