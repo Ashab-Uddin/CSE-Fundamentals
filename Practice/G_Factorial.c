0c3805a1177dfbe7aa71eebc2c4d89a06afb1ca3
Ashab Uddin <ashabuddin47@gmail.com>
2025-10-26 23:44:47 +0600

    trying to solve a problem

--- Practice/G_Factorial.c
+++ Practice/G_Factorial.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main(){
-   int n,m,x;
+   int n,m,x,f;
    scanf("%d %d %d",&n,&m,&x);
    for(int i=n;i>=0;i--){
     int f=n*n-1;
    }
+   printf("%d",f);
    
 
     return 0;
