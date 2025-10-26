2d56acb71492e3b86f9323aac289ebe006d8aae2
Ashab Uddin <ashabuddin47@gmail.com>
2025-10-27 00:33:09 +0600

    solve a problem palindromic or not

---
+++ Practice/H_One_Prime.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+   int n,prime =1;
+   scanf("%d",&n);
+   if(n<=1){
+    prime =0;
+   }
+   else{
+  for(int i=2;i<n;i++){
+    if(n%i == 0){
+        prime =0;
+        break;
+    }
+  }
+}
+if(prime ==1){
+    printf("YES");
+}
+else{
+    printf("NO");
+}
+
+    return 0;
+}
