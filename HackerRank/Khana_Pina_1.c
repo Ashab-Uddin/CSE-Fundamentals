c94b4f487e6d9028bdbf4a2e59f8bc5f5e5eb574
Ashab Uddin <ashabuddin47@gmail.com>
2025-10-28 10:30:07 +0600

    https://www.hackerrank.com/contests/practice-problem-a-introduction-to-c-programming-a-batch-08/challenges/khana-pina/problem

---
+++ HackerRank/Khana_Pina_1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+   int a;
+   scanf("%d",&a);
+   if(a>=1000){
+        printf("Three Kacchi");
+   }
+   else if(a>=500 && a<1000){
+        printf("One Large Pizza");
+   }
+   else if(a>=250 && a<500){
+        printf("Three Small Burger");
+   }
+   else if(a>=100 && a<500){
+        printf("Three Fuchka");
+   }
+   else{
+        printf("Nothing");
+   }
+    return 0;
+}
