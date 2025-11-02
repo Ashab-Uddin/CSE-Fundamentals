b6ae5085b8a90f3ad5136407de273f45e5eb5c14
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-03 02:35:17 +0600

    learn power function

--- Module-14/useful_math_function.c
+++ Module-14/useful_math_function.c
@@ -7,11 +7,13 @@ int main(){
     int r2 = round(4.6);
     int r3 = round(4.2);
     double s = sqrt(20);
+    int p = pow(3,2);
    printf("4.3 Ceilling Value:%d\n",c);
    printf("4.3 Floor Value:%d\n",f);
    printf("4.5 round valur:%d\n",r);
    printf("4.6 round value:%d\n",r2);
    printf("4.2 round value:%d\n",r3);
-   printf("20 square value:%lf",s);
+   printf("20 square value:%lf\n",s);
+   printf("3^2 power value:%d\n",p);
     return 0;
 }
