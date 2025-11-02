b9ef98db884d952fbad7dbb2edf8bc8ad79eb85e
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-03 02:19:16 +0600

    practice useful math function

---
+++ Module-14/useful_math_function.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int c = ceil(4.3);
+    int f = floor(4.3);
+   printf("4.3 Ceilling Value:%d\n",c);
+   printf("4.3 Floor Value:%d\n",f);
+    return 0;
+}
