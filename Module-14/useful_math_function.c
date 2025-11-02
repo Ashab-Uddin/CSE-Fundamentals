1d92ca00e8672273ccedc4967bdee00fd4c63143
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-03 02:31:14 +0600

    round function

--- Module-14/useful_math_function.c
+++ Module-14/useful_math_function.c
@@ -3,7 +3,13 @@
 int main(){
     int c = ceil(4.3);
     int f = floor(4.3);
+    int r = round(4.5);
+    int r2 = round(4.6);
+    int r3 = round(4.2);
    printf("4.3 Ceilling Value:%d\n",c);
    printf("4.3 Floor Value:%d\n",f);
+   printf("4.5 round valur:%d\n",r);
+   printf("4.6 round value:%d\n",r2);
+   printf("4.2 round value:%d\n",r3);
     return 0;
 }
