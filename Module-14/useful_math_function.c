02ccbc3c71b93ed4807ebc4868d4fe93ccc1d618
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-03 02:33:06 +0600

    squre function

--- Module-14/useful_math_function.c
+++ Module-14/useful_math_function.c
@@ -6,10 +6,12 @@ int main(){
     int r = round(4.5);
     int r2 = round(4.6);
     int r3 = round(4.2);
+    double s = sqrt(20);
    printf("4.3 Ceilling Value:%d\n",c);
    printf("4.3 Floor Value:%d\n",f);
    printf("4.5 round valur:%d\n",r);
    printf("4.6 round value:%d\n",r2);
    printf("4.2 round value:%d\n",r3);
+   printf("20 square value:%lf",s);
     return 0;
 }
