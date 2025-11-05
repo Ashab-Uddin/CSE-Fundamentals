dd2bf4e0f65a6b8657112ab0e5a79bafdcc3fb7a
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-05 23:59:28 +0600

    solve

--- Module-14.5/ultapyramid.c
+++ Module-14.5/ultapyramid.c
@@ -5,27 +5,21 @@ int main()
     int n;
     scanf("%d", &n);
 
-    int star = 1;
-    int space = n - 1;
+    int star = 2 * n - 1; // top row stars
+    int space = 0;        // no space at first
 
-         
     for (int i = 1; i <= n; i++)
     {
-        for (int k = 0; k < space; k++) 
-        {
+        for (int k = 0; k < space; k++)  // print spaces
             printf(" ");
-        }
 
-        for (int j = 1; j <= star; j++)
-        {
+        for (int j = 1; j <= star; j++)  // print stars
             printf("*");
-        }
 
         printf("\n");
-        star -= 2;
-        space++;
+        space++;      // increase spaces
+        star -= 2;    // decrease stars
     }
-
 
     return 0;
 }
