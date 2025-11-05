b0222fb784ca1d6d28406fee6ba7af33409a8099
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-05 23:58:29 +0600

    solve a ulta pyramid

--- Module-14.5/ultapyramid.c
+++ Module-14.5/ultapyramid.c
@@ -1,7 +1,31 @@
-#include<stdio.h>
-int main(){
-   int n;
-   scanf("%d",&n);
-   
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int star = 1;
+    int space = n - 1;
+
+         
+    for (int i = 1; i <= n; i++)
+    {
+        for (int k = 0; k < space; k++) 
+        {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= star; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+        star -= 2;
+        space++;
+    }
+
+
     return 0;
 }
