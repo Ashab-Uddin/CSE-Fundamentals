b1f2cc5185c76c254c70abc0a807e125e13ab45a
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-07 02:59:14 +0600

    pass by reference

--- Module-15/pass_by_reference.c
+++ Module-15/pass_by_reference.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
-void fun(int x){
-    x=20;
-    printf("fun value:%d\n",x);
-    printf("fun value address:%d\n",&x);
+void fun(int* p){
+    *p=20;
+    printf("fun value:%d\n",*p);
+    printf("fun value address:%d\n",p);
 
 }
 int main(){
    int x=10;
 //    scanf("%d",&x);
-   fun(x);
+printf(" main value:%d\n",x);
+   fun(&x);
    printf("main function value:%d\n",x);
    printf("main fun valude address:%p\n",&x);
     return 0;
