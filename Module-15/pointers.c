fa29bbdb4c817cb499956de0148b19889059b179
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-07 02:27:57 +0600

    learnign pointer

--- Module-15/pointers.c
+++ Module-15/pointers.c
@@ -2,10 +2,13 @@
 int main(){
    int x=10;
    printf("%d\n",x);
-   printf("%p\n",&x);
+   printf("%p\n",&x);//print the x address
    int* ptr;
    ptr = &x;
-   printf("%p\n",ptr);
-   printf("%p\n",&ptr);
+   printf("%p\n",ptr);//print the x variable address save as ptr
+   printf("%p\n",&ptr);//print the ptr variable address
+
+   *ptr =100;
+   printf("%d\n",*ptr);//print the value
     return 0;
 }
