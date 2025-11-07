499f55275af251f77040355d3b5d01c3f592483a
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-07 11:32:25 +0600

    print the array

--- Module-15/function_with_array.c
+++ Module-15/function_with_array.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
 void fun(int a[]){
     a[1] = 200;
+    
     printf("fun:%p\n",a);
 }
 int main(){
    int a[5]={10,20,30,40,50};
    printf("main:%p\n",a);
    fun(a);
+   for (int i = 0; i < 5; i++)
+   {
+    printf("%d ",a[i]);
+   }
+   
     return 0;
 }
