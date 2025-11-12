715ff0c47428495e7b61846ecce92ac0bb7e26a4
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-13 03:23:19 +0600

    solved this codeforces problem using recursion:https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/C

---
+++ Module-17/C_Print_from_N_to_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void printN(int i,int n){
+    if(n==0){
+        
+        return;
+    }
+    printf("%d ",n);
+    printN(i,n-1);
+}
+int main(){
+   int n;
+   scanf("%d",&n);
+   int i=1;
+   printN(i,n);
+    return 0;
+}
