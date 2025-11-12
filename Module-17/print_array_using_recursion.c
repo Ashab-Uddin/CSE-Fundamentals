987978dde882c1c0c7bc254fc1a3498f371e5b97
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-13 02:57:02 +0600

    wirte compelete code for print array using recursion

--- Module-17/print_array_using_recursion.c
+++ Module-17/print_array_using_recursion.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+void print_array(int a[],int n,int i){
+    if(i==n){
+        return;
+    }
+    printf("%d\n",a[i]);
+    print_array(a,n,i+1);
+}
 int main(){
    int n;
    scanf("%d",&n);
