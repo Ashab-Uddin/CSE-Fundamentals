845836a5221e5b881ec3b5dacef4edae4db292a8
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-13 03:43:05 +0600

    solved this codeforces problem using recursion:https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/F

--- Module-17/C_Print_from_N_to_1.c
+++ Module-17/C_Print_from_N_to_1.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
-void printN(int i,int n){
-    if(n==0){
-        
-        return;
-    }
-    printf("%d ",n);
-    printN(i,n-1);
+void printN(int n, int i){
+    if(i > n) return;
+
+    printN(n, i+1);
+
+    printf("%d", i);
+    if(i != 1) printf(" ");
 }
+
 int main(){
    int n;
    scanf("%d",&n);
-   int i=1;
-   printN(i,n);
+   
+   printN(n,1);
     return 0;
 }
---
+++ Module-17/F_Print_Even_Indices.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void reverse_array_print(int a[],int n,int i){
+    if(i==n){
+        return;
+    }
+    reverse_array_print(a,n,i+1);
+    if(i%2 == 0){
+        printf("%d ",a[i]);
+    }
+    
+}
+int main(){
+   int n;
+   scanf("%d",&n);
+   int a[n];
+   for(int i=0;i<n;i++){
+    scanf("%d",&a[i]);
+   }
+   reverse_array_print(a,n,0);
+    return 0;
+}
