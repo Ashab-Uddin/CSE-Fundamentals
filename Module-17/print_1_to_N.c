39f9aa47d01645d0b6294f5f3383f1c938e08394
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-13 00:28:22 +0600

    print 1 to n using recursion

--- Module-17/print_1_to_N.c
+++ Module-17/print_1_to_N.c
@@ -1,13 +1,24 @@
+// #include<stdio.h>
+// void hello(int i){
+//     if(i==6){
+//         return;
+//     }
+//     printf("%d\n",i);
+//     hello(i+1);
+// }
+// int main(){
+//    int i=1;
+//    hello(i);
+//     return 0;
+// }
+
 #include<stdio.h>
-void hello(int i){
-    if(i==6){
-        return;
-    }
-    printf("%d\n",i);
-    hello(i+1);
+void printN(int i,int n){
+    
 }
 int main(){
-   int i=1;
-   hello(i);
+   int i=1,n;
+   scanf("%d",&n);
+   printN(i,n);
     return 0;
 }
