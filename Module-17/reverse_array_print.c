8a6c5d4196521d1506c3553be34a51e998b4ae78
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-13 03:04:33 +0600

    compeleted code for reverse array print

--- Module-17/reverse_array_print.c
+++ Module-17/reverse_array_print.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+void reverse_array_print(int a[],int n,int i){
+    if(i==n){
+        return;
+    }
+    reverse_array_print(a,n,i+1);
+    printf("%d ",a[i]);
+}
 int main(){
    int n;
    scanf("%d",&n);
