08805a7f961e8efc8a5428cb3bbfe026b7b2dfa5
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-13 03:21:07 +0600

    solve this codeforces code using recursion:https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/B

---
+++ Module-17/B_Print_from_1_to_N.c
@@ -0,0 +1,29 @@
+// #include<stdio.h>
+// void hello(int i){
+//     if(i==6){
+//         return;
+//     }
+//     printf("%d\n",i);
+//     hello(i+1);
+// }
+// int main(){
+//    int i=1;
+//    hello(i);
+//     return 0;
+// }
+
+#include<stdio.h>
+void printN(int i,int n){
+    if(i==n){
+        printf("%d\n",n);
+        return;
+    }
+    printf("%d\n",i);
+    printN(i+1,n);
+}
+int main(){
+   int i=1,n;
+   scanf("%d",&n);
+   printN(i,n);
+    return 0;
+}
