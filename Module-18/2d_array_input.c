56f5b972ccda5bb1a382eed2b78f99b07b4c188b
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-13 19:10:14 +0600

    finaly write compeleted code to input and print 2d array

--- Module-18/2d_array_input.c
+++ Module-18/2d_array_input.c
@@ -7,6 +7,12 @@ int main(){
     for(int j=0;j<c;j++){
         scanf("%d",&a[i][j]);
     }
+   }
+
+   for(int i=0;i<r;i++){
+    for(int j=0;j<c;j++){
+        printf("%d ",a[i][j]);
+    }
     printf("\n");
    }
     return 0;
