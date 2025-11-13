44037041585b804ddfcaa5c669100faac63c762e
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-13 21:26:08 +0600

    find column matrix

---
+++ Module-18/column_matrix.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+
+{
+    int r, c;
+    scanf("%d %d", &r, &c);
+    int a[r][c];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    if(c==1){
+        printf("This is Column Matrix\n");
+    }
+    else{
+        printf("This is not Column Matrix\n");
+    }
+}
