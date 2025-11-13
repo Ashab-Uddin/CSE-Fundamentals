980b29e0565b6f8d136bd4a99794a308ffe0c280
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-14 01:39:39 +0600

    solve this factorial find problem

---
+++ Module-19/J_Factorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+long long int fact(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    long long int mul = fact(n - 1);
+    return mul*n;
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    long long int factResult = fact(n);
+    printf("%lld",factResult);
+    return 0;
+}
