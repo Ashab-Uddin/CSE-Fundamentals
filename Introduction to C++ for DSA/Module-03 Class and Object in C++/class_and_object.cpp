6c613decdac02ff8f5544d838432ea6f596dcfc6
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-22 20:22:51 +0600

    input two person info

--- Introduction to C++ for DSA/Module-03 Class and Object in C++/class_and_object.cpp
+++ Introduction to C++ for DSA/Module-03 Class and Object in C++/class_and_object.cpp
@@ -18,9 +18,12 @@ int main(){
     // input with space
     cin.getline(a.name,100);
     cin>>a.roll>>a.gpa;
-    // cin>>b.name>>b.roll>>b.gpa;
+    //remove enter
+    cin.ignore();
+    cin.getline(b.name,100);
+    cin>>b.roll>>b.gpa;
 
     cout<<a.name<<" "<<a.roll<<" "<<a.gpa<<endl;
-    // cout<<b.name<<" "<<b.roll<<" "<<b.gpa<<endl;
+    cout<<b.name<<" "<<b.roll<<" "<<b.gpa<<endl;
     return 0;
 }
