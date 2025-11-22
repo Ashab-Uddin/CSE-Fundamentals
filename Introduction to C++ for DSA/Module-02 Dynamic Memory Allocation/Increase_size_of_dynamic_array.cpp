f0824b1de002c5caf13f0ab6481c91506a828dea
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-22 19:31:58 +0600

    learn how to increase dynamic array in c++

--- Introduction to C++ for DSA/Module-02 Dynamic Memory Allocation/Increase_size_of_dynamic_array.cpp
+++ Introduction to C++ for DSA/Module-02 Dynamic Memory Allocation/Increase_size_of_dynamic_array.cpp
@@ -1,11 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int a[3];
+    // int a[3];
+    int *a = new int[3];
     for(int i=0;i<3;i++){
         cin>>a[i];
     }
-    int b[5];
+    int*b = new int[5];
     for(int i=0;i<3;i++){
         b[i]=a[i];
     }
