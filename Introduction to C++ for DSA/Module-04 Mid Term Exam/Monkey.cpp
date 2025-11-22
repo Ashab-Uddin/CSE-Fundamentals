0c4f4ce136cc6b200e2b5ba1e33e81801c34b714
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-22 22:47:09 +0600

    solve this problem form haccker rank:https://www.hackerrank.com/contests/mid-term-a-introduction-to-c-for-dsa-a-batch-08/challenges/monkey-1-2

--- Introduction to C++ for DSA/Module-04 Mid Term Exam/Monkey.cpp
+++ Introduction to C++ for DSA/Module-04 Mid Term Exam/Monkey.cpp
@@ -3,11 +3,18 @@ using namespace std;
 
 int main() {
     char s[100001];
-    
+
     while (cin.getline(s, 100001)) {
         int len = strlen(s);       
         sort(s, s + len);          
-        cout << s;         
+        for (int i = 0; i < len; i++)
+        {
+            if (s[i] != ' ')
+            {
+                cout << s[i];
+            }
+        }
+        cout << endl;         
     }
 
     return 0;
