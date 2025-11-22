a8ed276d3574de820f03e455147e6ebf05a20fab
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-22 21:04:08 +0600

    learned this keyword

---
+++ Introduction to C++ for DSA/Module-03 Class and Object in C++/3_6_this_array_sign.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student{
+    public:
+    int roll;
+    int cls;
+    double gpa;
+
+    // constractor
+    Student(int roll,int cls,double gpa){
+        this->roll = roll;
+        this->cls = cls;
+        this->gpa =gpa;
+    }
+};
+int main(){
+    Student Ashab(6,10,4.52);
+    Student Sohel(7,10,4.65);
+
+    cout<<Ashab.roll<<" "<<Ashab.cls<<" "<<Ashab.gpa<<endl;
+    cout<<Sohel.roll<<" "<<Sohel.cls<<" "<<Sohel.gpa<<endl;
+}
