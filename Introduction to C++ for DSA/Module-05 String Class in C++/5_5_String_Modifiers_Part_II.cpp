3f625e07c5a39e3e14b9cf3ef122a81b7587ff48
Ashab Uddin <ashabuddin47@gmail.com>
2025-12-18 15:33:57 +0600

    erase()->erase the string selected index to end

--- Introduction to C++ for DSA/Module-05 String Class in C++/5_5_String_Modifiers_Part_II.cpp
+++ Introduction to C++ for DSA/Module-05 String Class in C++/5_5_String_Modifiers_Part_II.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 int main(){
     //assign string
-    string s = "Hello";
-    string s2 = "Gello";
+    string s = "Hello World";
+    // string s2 = "Gello";
     // s = s2;
-    s.assign(s2);//same as s=s2;
-    
+    //s.assign(s2);//same as s=s2;
+    //erase(start index)function erase the at last
+    s.erase(5); 
+
     cout<<s<<endl;
     return 0;
 }
