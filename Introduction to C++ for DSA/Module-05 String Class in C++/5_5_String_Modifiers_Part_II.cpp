9fb8386392a9b8fe83c846d707d10623afafac52
Ashab Uddin <ashabuddin47@gmail.com>
2025-12-18 15:26:34 +0600

    learn string assign function

---
+++ Introduction to C++ for DSA/Module-05 String Class in C++/5_5_String_Modifiers_Part_II.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //assign string
+    string s = "Hello";
+    string s2 = "Gello";
+    s = s2;
+    cout<<s<<endl;
+    return 0;
+}
